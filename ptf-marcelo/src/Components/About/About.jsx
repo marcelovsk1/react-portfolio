097bb13acd2a85e9c917c9567b322ac8cd7d7765
Marcelo Amaral Alves <marceloaa@live.com>
2024-03-18 14:43:44 -0300

    working on

--- ptf-marcelo/src/Components/About/About.jsx
+++ ptf-marcelo/src/Components/About/About.jsx
@@ -6,40 +6,41 @@ import iOS from '../../../src/img/ios_2.png'
 
 const About = () => {
   return (
-    <section className='styles_container' id='about'>
-      <h2 className='styles_title'>About <span>Me</span></h2>
-      <div className='styles_content'>
-      <div className="pic_container">
-        <img src={About_Pic} alt="About_Pic" className='About-Pic' />
-        <ul className='styles_itens'>
-          <li className='education'>
-            <div className='webdev'>
-              <div className='description'>
-                <h3>Web-Developer</h3>
-                <p>Full Stack Web Developer graduated from Le Wagon Montreal. With a solid foundation in both front-end and back-end technologies, I am dedicated to crafting impactful and functional digital experiences.</p>
-              </div>
-            </div>
-              <a href="https://www.lewagon.com">
-                <img src={le_wagon} alt="Le Wagon" className='le_wagon' />
-              </a>
-          </li>
-          <li className='education'>
-            <div className="webdev">
-              <div className="description">
-                <h3>iOS Developer</h3>
-                <p>Following my bootcamp graduation, I delved into iOS development, honing my skills and specializing in crafting seamless experiences for Apple's ecosystem.</p>
-            </div>
-              </div>
-              <a href="https://www.linkedin.com/feed/update/urn:li:activity:7143249482875396096/">
-                <img src={iOS} alt="iOS" className='iOS' />
-              </a>
-          </li>
-        </ul>
+    <div class="container-wrapper">
+      <section class='styles_container' id='about'>
+        <h2 class='styles_title'>About <span>Me</span></h2>
+        <div class='styles_content'>
+          <div class="pic_container">
+            <img src={About_Pic} alt="About_Pic" class='About-Pic' />
+            <ul class='styles_itens'>
+              <li class='education'>
+                <div class='webdev'>
+                  <div class='description'>
+                    <h3>Web-Developer</h3>
+                    <p>Full Stack Web Developer graduated from Le Wagon Montreal. With a solid foundation in both front-end and back-end technologies, I am dedicated to crafting impactful and functional digital experiences.</p>
+                  </div>
+                </div>
+                <a href="https://www.lewagon.com">
+                  <img src={le_wagon} alt="Le Wagon" class='le_wagon' />
+                </a>
+              </li>
+              <li class='education'>
+                <div class="webdev">
+                  <div class="description">
+                    <h3>iOS Developer</h3>
+                    <p>Following my bootcamp graduation, I delved into iOS development, honing my skills and specializing in crafting seamless experiences for Apple's ecosystem.</p>
+                  </div>
+                </div>
+                <a href="https://www.linkedin.com/feed/update/urn:li:activity:7143249482875396096/">
+                  <img src={iOS} alt="iOS" class='iOS' />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
-
 
 export default About
